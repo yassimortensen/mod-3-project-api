6e7ee588937af6f6f3d69e8f2138c027add66367
github username <github email address>
2017-12-05 14:57:10 -0500

    nokogiri scrape

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,12 +3,22 @@ doc = Nokogiri::HTML(open("http://www.pajiba.com/tv_reviews/game-of-thrones-hot-
 
 div = doc.css('div[style="padding-bottom:1200px;"]')
 
-a_div = div
+body_clone = div.clone
+body_div = body_clone.search("//a").remove
+body = body_clone.first.text
 
-body_div = div
+a_div_clone = div.clone
+anchors = a_div_clone.css('a')
+anchor_id = anchors.css('a[id]')
+anchor_href = a_div_clone.css('a[href]')
 
-remove_a =  body_div.search("//a").remove
-
-body = body_div.text
 
 puts body
+puts anchors
+puts anchor_id
+puts anchor_href
+
+
+
+# body_array = []
+# body_div.each{|text| body_array.push(text.text)}
