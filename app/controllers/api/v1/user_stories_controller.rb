6587e97d6894d73b76749154ccc3efd4fa7b5a33
yassimortensen <yassimortensen@gmail.com>
2017-12-04 22:56:37 -0500

    updated controller actions

--- app/controllers/api/v1/user_stories_controller.rb
+++ app/controllers/api/v1/user_stories_controller.rb
@@ -1,2 +1,23 @@
 class Api::V1::UserStoriesController < ApplicationController
+
+  def index
+    @userstories = UserStory.all
+    render json: @userstories
+  end
+
+  def create
+    @userstory = UserStory.new(userstory_params)
+    if @userstory.save
+      render json: @userstory
+    else
+      render index.html
+    end
+  end
+
+  private
+
+  def userstory_params
+    params.permit(:userId, :storyStageId)
+  end
+
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,13 +6,18 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def create
-    # @user = User.new(params)
-    #
-    # if @user.save
-    #   render json: @note
-    # else
-    #   render index.html
-    # end
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user
+    else
+      render index.html
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :characterName, :favFood, :firstJob, :favAnimal)
   end
 
 end
