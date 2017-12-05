9f25c8bc02b357b7c9236995b868eaf7df308a03
github username <github email address>
2017-12-05 10:50:47 -0500

    testing scraping

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
-#create pets using above array
- #create Pet objects for each
+doc = Nokogiri::HTML(open("http://www.pajiba.com/tv_reviews/game-of-thrones-hot-pies-choose-your-own-adventure.php"))
+
+div = doc.css('div[style="padding-bottom:1200px;"]')
+
+a_div = div
+
+body_div = div
+
+remove_a =  body_div.search("//a").remove
+
+body = body_div.text
+
+puts body
