b22ea90fa383d48a9c98a6b719db3a135dcf9d9f
github username <github email address>
2017-12-05 15:52:11 -0500

    story stage creator

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,20 +3,47 @@ doc = Nokogiri::HTML(open("http://www.pajiba.com/tv_reviews/game-of-thrones-hot-
 
 div = doc.css('div[style="padding-bottom:1200px;"]')
 
-body_clone = div.clone
-body_div = body_clone.search("//a").remove
-body = body_clone.first.text
+# a_div_clone = div.clone
+# anchors = a_div_clone.css('a')
+# anchor_id = anchors.css('a[id]')
+# anchor_href = a_div_clone.css('a[href]')
 
-a_div_clone = div.clone
-anchors = a_div_clone.css('a')
-anchor_id = anchors.css('a[id]')
-anchor_href = a_div_clone.css('a[href]')
+# body_clone = div.clone
+# body_div = body_clone.search("//a").remove
+# body = body_clone.first.text
+
+div.each do |tag|
+  tag.css('a[href]').each do |href|
+    href.each do
+      stage = tag.children[2].attributes["id"].value
+      body = href.children.text
+      button_txt = href.attributes["href"].value #this is just to slice correctly
+      button_slc = button_txt.slice!(0)
+      next_step = href.attributes["href"].value
+      description = tag.children[4].text.strip
+      new_stage = StoryStage.new
+      byebug
+    end
+  end
+end
+
+# t.string :stage
+# t.string :body
+# t.string :button
+# t.string :nextStep
+# t.string :description
+# t.integer :pic_id
+
+# next_step = href.attributes["href"].value
+# description = tag.children[4].text.strip
+# body = href.children.text
+# step = tag.children[2].attributes["id"].value
 
 
-puts body
-puts anchors
-puts anchor_id
-puts anchor_href
+# puts body
+# puts anchors
+# puts anchor_id
+# puts anchor_href
 
 
 
