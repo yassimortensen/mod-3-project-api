6d8027bf624d39ba9252b93b2adf007713680787
github username <github email address>
2017-12-06 17:15:18 -0500

    edit seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,36 +1,36 @@
-# def story_stage()
-#   doc = Nokogiri::HTML(open("http://www.pajiba.com/tv_reviews/game-of-thrones-hot-pies-choose-your-own-adventure.php"))
-#   div = doc.css('div[style="padding-bottom:1200px;"]')
-#   # a_div_clone = div.clone
-#   # anchors = a_div_clone.css('a')
-#   # anchor_id = anchors.css('a[id]')
-#   # anchor_href = a_div_clone.css('a[href]')
-#   # body_clone = div.clone
-#   # body_div = body_clone.search("//a").remove
-#   # body = body_clone.first.text
-#
-#   div.each do |tag|
-#     tag.css('a[href]').each do |href|
-#       href.each do
-#         body = href.children.text
-#         button_txt = href.attributes["href"].value #this is just to slice correctly
-#         button_slc = button_txt.slice!(0)
-#         next_step = href.attributes["href"].value
-#         begin
-#           stage = tag.css('a[id]')[0].attributes["id"].value
-#         rescue NoMethodError
-#           byebug
-#         end
-#         description_remove = tag.children.text
-#         description_array = description_remove.split(/[\n]+/)
-#         description = description_array[1]
-#         # byebug
-#         new_stage = StoryStage.new(stage: stage, body: body, button: button_txt, nextStep: next_step, description: description, pic_id: 1)
-#         new_stage.save
-#       end
-#     end
-#   end
-# end
+def story_stage()
+  doc = Nokogiri::HTML(open("http://www.pajiba.com/tv_reviews/game-of-thrones-hot-pies-choose-your-own-adventure.php"))
+  div = doc.css('div[style="padding-bottom:1200px;"]')
+  # a_div_clone = div.clone
+  # anchors = a_div_clone.css('a')
+  # anchor_id = anchors.css('a[id]')
+  # anchor_href = a_div_clone.css('a[href]')
+  # body_clone = div.clone
+  # body_div = body_clone.search("//a").remove
+  # body = body_clone.first.text
+
+  div.each do |tag|
+    tag.css('a[href]').each do |href|
+      href.each do
+        body = href.children.text
+        button_txt = href.attributes["href"].value #this is just to slice correctly
+        button_slc = button_txt.slice!(0)
+        next_step = href.attributes["href"].value
+        begin
+          stage = tag.css('a[id]')[0].attributes["id"].value
+        rescue NoMethodError
+          byebug
+        end
+        description_remove = tag.children.text
+        description_array = description_remove.split(/[\n]+/)
+        description = description_array[1]
+        # byebug
+        new_stage = StoryStage.new(stage: stage, body: body, button: button_txt, nextStep: next_step, description: description, pic_id: 1)
+        new_stage.save
+      end
+    end
+  end
+end
 
 
 
@@ -111,23 +111,3 @@ def pics()
     new_pic.save
   end
 end
-<<<<<<< HEAD
-=======
-
-
-# div.each do |tag|
-#   tag.css('a[href]').each do |href|
-#     href.each do
-#       byebug
-#       stage = tag.children[2].attributes["id"].value
-#       body = href.children.text
-#       button_txt = href.attributes["href"].value #this is just to slice correctly
-#       button_slc = button_txt.slice!(0)
-#       next_step = href.attributes["href"].value
-#       description = tag.children[4].text.strip
-#       new_stage = StoryStage.new(stage: stage, body: body, button: button_txt, nextStep: next_step, description: description, pic_id: 1)
-#       new_stage.save
-#     end
-#   end
-# end
->>>>>>> 6be68ae366a85d5406c2a9b5fdae00b2f2f775a5
