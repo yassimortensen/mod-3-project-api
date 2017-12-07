d42c0439ec4452e3a177a64bb2d7b895fa879748
yassimortensen <yassimortensen@gmail.com>
2017-12-07 17:28:11 -0500

    fixed big

--- db/seeds.rb
+++ db/seeds.rb
@@ -138,18 +138,12 @@ def story_pics
   end
 end
 
-# 2.times do
-#   byebug
-#   pics()
-# end
-#
-=======
 2.times do
 
   pics()
 end
 
->>>>>>> 0b3c077cc51eece491327a8ff6db3ed1e6ee3117
+
 story_stage()
 # story_pics()
 
